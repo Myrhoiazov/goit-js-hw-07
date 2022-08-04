1f7ba8b7688b20e92e63b0f98ebadda6c7c8b163
Myrhoiazov <denis.mirgoyazov@gmail.com>
2022-08-04 23:49:14 +0300

    1 practice

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,63 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+{
+  /* <div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="small-image.jpg"
+      data-source="large-image.jpg"
+      alt="Image description"
+    />
+  </a>
+</div> */
+}
+
+// console.log(galleryItems);
+
+const galleryRef = document.querySelector('.gallery');
+
+const item = galleryItems.reduce((acc, elem) => {
+  return (acc += `<div class="gallery__item">
+    <a class="gallery__link" href="${elem.original}">
+      <img
+        class="gallery__image"
+        src="${elem.preview}"
+        data-source="${elem.original}"
+        alt="${elem.description}"
+      />
+    </a>
+  </div> `);
+}, '');
+
+galleryRef.insertAdjacentHTML('beforeend', item);
+
+galleryRef.addEventListener('click', handleclickImage);
+
+function handleclickImage(event) {
+  if (!event.target.classList.contains('gallery__image')) {
+    return;
+  }
+
+  event.preventDefault();
+
+  const modalMarkUp = `<img width="960" src="${event.target.dataset.source}">`;
+
+  const instance = basicLightbox.create(modalMarkUp, {
+    onShow: () => {
+      window.addEventListener('keydown', onKeyDown);
+    },
+    onClose: () => {
+      window.removeEventListener('keydown', onKeyDown);
+    },
+  });
+
+  function onKeyDown(event) {
+    if (event.code === 'Escape') {
+      instance.close();
+    }
+  }
+
+  instance.show();
+}
