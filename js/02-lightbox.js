35da43c2e92e52a7c6aeb7a6e9c7409e9fdb5ddd
Myrhoiazov <denis.mirgoyazov@gmail.com>
2022-08-05 13:11:37 +0300

    add: correct

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,38 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+{
+  /* <a class="gallery__item" href="large-image.jpg">
+  <img class="gallery__image" src="small-image.jpg" alt="Image description" />
+</a> */
+}
+
+// console.log(galleryItems);
+
+const galleryRef = document.querySelector('.gallery');
+
+const li = galleryItems
+  .map((elem) => {
+    return `<li class="gallery__item">
+    <a class="gallery__link" href="${elem.original}">
+    <img class="gallery__image" src="${elem.preview}" alt="${elem.description}" />
+  </a> 
+    </li> `;
+  })
+  .join('');
+
+galleryRef.innerHTML = li;
+
+galleryRef.addEventListener('click', handleclickImage);
+let gallery = new SimpleLightbox('.gallery a', {'captionsData': 'alt', 'captionDelay': 250});
+
+
+function handleclickImage(event) {
+
+  if (!event.target.classList.contains('gallery__image')) {
+    return;
+  }
+
+  event.preventDefault();
+
+}
